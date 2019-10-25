65df8af651389314f59bf1605b719ca4c762a54a
Lucas Martines <lucas.alcantarilla97@gmail.com>
2019-10-24 22:04:55 -0300

    adicionado driver

--- resources/views/dia/mostrar_todos.blade.php
+++ resources/views/dia/mostrar_todos.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('titulo',"Bem vindo")
 @section("content")
+<!-- controles -->
 <div class="mt-3  d-flex">
     <h4 class="pt-2 mr-auto" >
         Dias de estudo
@@ -10,28 +11,38 @@
         Criar um Dia
     </a>
 </div> 
-
- <ul class="mt-3">
+<!-- painel -->
+<div class="row">
+    <!-- left panel -->
+    <ul class="mt-3 col-12 col-lg-7  order-2 order-md-1" >
     @foreach($dias as $dia)
         <li class="mt-1 mb-3" >
             <h2> {{$dia->titulo}} </h2>
             <p> data:  {{$dia->created_at}}</p>
-            <a href="/dia/{{$dia->id}}">Ver Detalhes dos Estudos</a>
-            
-                <a  
-                    href="/dia/{{$dia->id}}/deletar"
-                    value="deletar"
-                    class="text-danger">
-                    Deletar
-                </a>
-                
-             
+
+            <a href="/dia/{{$dia->id}}">
+                Ver Detalhes dos Estudos
+            </a>
+
+            <a  
+                href="/dia/{{$dia->id}}/deletar"
+                value="deletar"
+                class="text-danger">
+                Deletar
+            </a>               
         </li>
     @endforeach
     @if( empty($dias) )
         <p>Nenhum dia </p>
     @endif
-</ul>
+    </ul>
+    <div class="col-lg-5 col-12 order-1 order-md-2">
+        <div class="d-flex align-items-center justify-content-center">
+            <img src="{{asset('imagens/crianca_estudando.webp')}}" alt="">
+        </div>
+    </div>
+</div>
+<!-- paginação -->
 <div class="text-center">
 {{ $dias->links() }}
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.app_full')
 @section('titulo',"Bem vindo")
 @section("content")
-<h2 class="mt-4">
-    Seja bem vindo a aplicação de diario de estudos
-</h2>
+<div class="w-100 p-0 imagemBemvindo">
+    <div class="img">
+
+    </div>
+    <div class="gradient">
+
+    </div>
+    <h2 class="mt-4">
+        Seja bem vindo ao diario de estudos
+    </h2>
+</div>
+<div class="container mt-3">
+    <h5> Aplicação de registro de estudos </h5>
+</div>
 @endsection
